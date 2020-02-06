5174822e6aa00366beaa6429f1808a142e5d4c1e
Matt Brucker <mpbrucker@gmail.com>
2020-02-05 21:57:17 -0500

    Creating sum_array function

--- exercises/ex02/adder.c
+++ exercises/ex02/adder.c
@@ -3,15 +3,30 @@
 #include <string.h>
 
 int input_to_int(char * input) {
+    
+}
 
+/*
+    Sums thhe elements of an int array, and returns the total
+
+    array: an integer array containing the numbers to sum
+    array_len: the length of the array
+*/
+int sum_array(int * array, int array_len) {
+    int total = 0;
+    for (int i=0; i<array_len; i++) {
+        total += array[i];
+    }
+    return total;
 }
 
 int main() {
     int nums[20] = {0};
     char user_input[12];
-
-    do {
-        puts("Inter an integer: ");
-        scanf("%11s", user_input);
-    }  while (user_input != EOF);
+    puts("Enter an integer: ");
+    while (fgets(user_input, sizeof(user_input), stdin) != NULL) {
+        puts("Enter an integer: ");
+        int input_int = atoi(user_input);
+    } 
+    printf("Total: %d", sum_array(nums, sizeof(nums)))
 }
