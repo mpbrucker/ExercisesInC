dde89094ea30ca4ae9a418a981ec3f5c15524491
Matt Brucker <mpbrucker@gmail.com>
2020-02-05 22:27:48 -0500

    Fixing issue with array length check.

--- exercises/ex02/adder.c
+++ exercises/ex02/adder.c
@@ -16,12 +16,33 @@ int sum_array(int * array, int array_len) {
     int total = 0;
     for (int i=0; i<array_len; i++) {
         total += array[i];
+        printf("%d\n", total);
     }
     return total;
 }
 
+/* 
+    Checks to see whether the input buffer is "overfull" (i.e., if the array 
+    is full and the second to last character isn't \n.)
+    Returns 1 if full, 0 otheriwse
+
+    array: The input buffer
+    size: the expected max input size
+*/
+int check_input_full(char * array, int size) {
+    if (strlen(array) == (size-1) && array[size-2] != '\n') {
+            printf("Error: input is too long");
+            return 1; // TODO: Should it keep going?
+    }
+    return 0;
+}
+
 int main() {
-    int nums[20] = {0};
+    int nums[20];
+    for (int i=0; i<20; i++) {
+        nums[i] = 0;
+    }
+    
     char user_input[13]; // TODO: Determine actual maximum length
     int i = 0;
     void * input_success;
@@ -29,11 +50,20 @@ int main() {
     do {
         puts("Enter an integer: ");
         input_success = fgets(user_input, 13, stdin);
-        if (strlen(user_input) == 12 && user_input[11] != '\n') {
-            printf("Error: input is too long");
-            break; // TODO: Should it keep going?
+        if (check_input_full(user_input, 13)) {
+            break;
         }
         int input_int = atoi(user_input);
+        if (input_int == 0) {
+            printf("Error: zero value or non-integer entered.");
+            continue;
+        }
+        nums[i] = input_int;
+        i++;
+        if (i >= sizeof(nums)/4) {
+            printf("Maximum number of integers entered.");
+            break;
+        }
     } while (input_success != NULL);
-    printf("Total: %d", sum_array(nums, sizeof(nums)));
+    printf("Total: %d", sum_array(nums, sizeof(nums)/4));
 }
