f5595e8f652ede8749217683e91a4421539d1143
Matt Brucker <mpbrucker@gmail.com>
2020-02-19 17:39:54 -0500

    Removing attempt to handle signals

--- exercises/ex03/tee.c
+++ exercises/ex03/tee.c
@@ -1,46 +1,28 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <signal.h>
-int flag = 0;
-int ignore_interrupt = 0;
 
 void handle_input(FILE * streams[], int num_streams) {
     size_t line_size = 0;
     char * buffer = NULL;
 
     while(1) {
-        if (flag) {
-            flag = 0;
-            break;
-        }
         getline(&buffer, &line_size, stdin);
         printf("%s", buffer);
         for (int i=0; i < num_streams; i++) {
+            printf("%d", i);
             fprintf(streams[i], "%s\n", buffer);
         }
-    }
-}
-
-void handle_interrupt(int sig) {
-    puts("interrupt");
-    if (!ignore_interrupt) {
-        flag = 1;
     }
 }
 
 int main(int argc, char * argv[]) {
     char ch;
     int append = 0;
-    signal(SIGINT, handle_interrupt);
 
-    while ((ch = getopt(argc, argv, "aipohv")) != EOF) {
+    while ((ch = getopt(argc, argv, "apohv")) != EOF) {
         switch (ch) {
             case 'a':
                 append = 1;
-                break;
-            case 'i':
-                puts("test");
-                ignore_interrupt = 1;
                 break;
             case 'p':
                 break;
