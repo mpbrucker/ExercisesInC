72ee9d38fdf4c0d56512826938c75982818938b5
Matt Brucker <mpbrucker@gmail.com>
2020-02-19 21:58:01 -0500

    Fix issue with append on multiple files.

--- exercises/ex03/tee.c
+++ exercises/ex03/tee.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <string.h>
 
 
 //  Opens a file stream with a given file name in a specified mode, writes a buffer to it, and closes the filestream.
@@ -10,9 +9,13 @@
 //
 void write_stream(char * stream_name, char * mode, char * buffer) {
     FILE * stream; 
+    int res;
     stream = fopen(stream_name, mode);
-    fprintf(stream, "%s", buffer);
+    res = fprintf(stream, "%s", buffer);
     fclose(stream);
+    if (stream == NULL || res == -1) {
+        printf("Error writing to file: %s", stream_name);
+    }
 }
 
 //  Handles the input loop of taking in input writing anad writing it to a list of filestreams.
@@ -24,20 +27,29 @@ void handle_input(char * stream_names[], int num_streams, char * append) {
     size_t line_size = 0;
     char * buffer = NULL;
     int has_opened = 0;
+    int res;
 
     while(1) {
-        getline(&buffer, &line_size, stdin);
+        res = getline(&buffer, &line_size, stdin);
+        if (res == -1) {
+            printf("Error reading input from stdin");
+            break;
+        }
         printf("%s", buffer);
         for (int i=0; i < num_streams; i++) {
             write_stream(stream_names[i], has_opened ? "a" : append, buffer);
-            if (!has_opened) {
-                has_opened = !has_opened;
-            }
+        }
+        if (!has_opened) {
+            has_opened = !has_opened;
         }
     }
     
 }
 
+// Writes input from stdin to one or more filestreams whose filenames are provided.
+// -a option: append to files (instead of writing)
+// -v option: version
+// 
 int main(int argc, char * argv[]) {
     char ch;
     int append = 0;
@@ -48,7 +60,7 @@ int main(int argc, char * argv[]) {
                 append = 1;
                 break;
             case 'v':
-                puts("tee version 0.1.");
+                printf("%s", "tee version 0.1.");
                 break;
             default:
                 printf("tee: invalid option -- '%c'\nTry 'tee --help' for more information.\n", ch);
